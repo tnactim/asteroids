58fd3c47a2695b97efc44952ea918b1515cd0064
Tim Horner <tnactim@gmail.com>
2025-06-12 22:05:05 -0400

    AsteroidField class implemented and asteroids are spawning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
 	print("Starting Asteroids!")
@@ -12,9 +14,13 @@ def main():
 	dt = 0
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
 	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-	updatable.add(player)
+	asteroids = AsteroidField()
+	
 
 	# game loop
 	while True:
